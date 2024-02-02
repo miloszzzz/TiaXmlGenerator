3c01adf2f2c93d11c23e2bef7232e8bd0ed58598
miloszzzz <1picipolo@gmail.com>
2024-02-02 10:43:12 +0100

    screens to textlist added

---
+++ TiaXmlGenerator/Models/AddinScreen.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TiaXmlGenerator.Models
+{
+    public class AddinScreen
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+
+        public AddinScreen(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+
+        public AddinScreen(int id, string name, string description)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+        }
+    }
+
+
+    
+}
